7a39c118fbc17cb1715d575a39afe2a443e45d9d
denchen11214 <dchen22@stuy.edu>
2019-02-03 18:21:00 -0500

    finished

--- 02_canvas/dotconn.js
+++ 02_canvas/dotconn.js
@@ -7,16 +7,23 @@ var canvas = document.getElementById('playground');
 var ctx = canvas.getContext('2d');
 var clear = document.getElementById("clear");
 clear.addEventListener('click', function(e){
+    //clears the rectangle and starts a new path so that a line isn't created on the 1st ellipse
     ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.beginPath();
 })
 canvas.addEventListener('click', function(e){
+    //gets the x and y value of the mouse with respect to the canvas
     var x = e.offsetX;
     var y = e.offsetY;
+    //makes a line from the previous ellipse to the new mouse location
+    ctx.lineTo(x,y);
+    ctx.stroke();
+    //creates an ellipse where the mouse is
     ctx.beginPath();
     ctx.ellipse(x, y, 50, 50, 0, 0, 2 * Math.PI);
-    ctx.moveTo(x,y);
-    ctx.lineTo(x,y);
     ctx.closePath();
     ctx.stroke();
     ctx.fill();
+    //moves the context to the new location for future lines
+    ctx.moveTo(x,y)
 })
