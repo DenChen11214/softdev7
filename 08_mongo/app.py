da73a601b376dc5efe217ca2ae809296b2008756
denchen11214 <dchen22@stuy.edu>
2019-03-07 21:32:29 -0500

    finished

--- 08_mongo/app.py
+++ 08_mongo/app.py
@@ -8,6 +8,8 @@ import pymongo
 import os
 import json
 
+import mongo
+
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 collection = None
@@ -16,93 +18,46 @@ collection = None
 def home():
     return render_template('main.html')
 
-@app.route('/display', methods = ["POST"])
+@app.route('/display', methods = ["GET","POST"])
 def display():
     SERVER_ADDR = request.form['serverid']
-    connection=pymongo.MongoClient(SERVER_ADDR)
-    db=connection.GarlicHumus
-    collection=db.senators
-    f = open("senators.json","r")
-    data = json.loads(f.read())
-    f.close()
-    collection.insert_many(data)
-    return render_template('newpage.html')
+    global collection
+    connection = mongo.connect(SERVER_ADDR)
+    collection = mongo.importDB(SERVER_ADDR)
+    return render_template("newpage.html")
 
 @app.route('/getSenP', methods = ["POST"])
 def senParty():
     party = request.form['party']
-    people = get_senators_from_party(party)
+    print(collection)
+    people = mongo.get_senators_from_party(party)
     return render_template("info.html",info = people)
 @app.route('/getSenI', methods = ["POST"])
 def senInfo():
-    name = request.form["Senator's name"]
-    info = get_senator_info(name)
+    name = request.form["name"]
+    info = mongo.get_senator_info(name)
     return render_template("info.html",info = info)
 
 @app.route('/getConI', methods = ["POST"])
 def senContact():
-    name = request.form["Senator's name"]
-    contact = get_contact_info(name)
+    name = request.form["name"]
+    contact = mongo.get_contact_info(name)
     return render_template("info.html",info = contact)
 
 @app.route('/getSMI', methods = ["POST"])
 def senSocial():
-    name = request.form["Senator's name"]
-    info = get_social_media_info(name)
+    name = request.form["name"]
+    info = mongo.get_social_media_info(name)
     return render_template("info.html",info = info)
 
 @app.route('/getSenS', methods = ["POST"])
 def senState():
     state = request.form["State"]
-    senators = get_senators_from_state(state)
+    senators = mongo.get_senators_from_state(state)
     return render_template("info.html",info = senators)
-    
-#---------------------------------------------------------------------------------
 
-def get_senators_from_party(party):
-        results = collection.find({"party":party})
-        print("Senators from the " + party + " party: ")
-        for res in results:
-            print(res['person']['name'])
-def get_senator_info(person):
-    name = str.split(person)
-    print("Basic Info of " + name+ " :")
-    results = collection.find({"person.firstname":name[0],"person.lastname":name[1]})
-    for res in results:
-        print("Name: " + res['person']['name'])
-        print("Gender: " + res['person']['gender'])
-        print("Birthday: " + res['person']['birthday'])
-        print("Description: " + res['description'])
-        print("State: " + res['state'])
-    if (results.count() == 0):
-        print("Senator Not Found")
-def get_contact_info(person):
-    name = str.split(person)
-    results = collection.find({"person.firstname":name[0],"person.lastname":name[1]})
-    print("Contact Info of " + person+ " :")
-    for res in results:
-        print("Email: " + res['extra']['contact_form'])
-        print("Fax: " + res['extra']['fax'])
-        print("Office Address: " + res['extra']['address'])
-    if (results.count() == 0):
-        print("Senator Not Found")
-def get_social_media_info(person):
-    name = str.split(person)
-    results = collection.find({"person.firstname":name[0],"person.lastname":name[1]})
-    print("Social Media of " + person+ " :")
-    for res in results:
-        print("Youtube: " + res['person']['youtubeid'])
-        print("Twitter: " + res['person']['twitterid'])
-        print("Website: " + res['website'])
-    if (results.count() == 0):
-        print("Senator Not Found")
-def get_senators_from_state(state):
-    print("Senators from " + state + " :")
-    results = collection.find({"state":state})
-    for res in results:
-        print(res['person']['name'])
 
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host = "0.0.0.0")
+    app.run()
