151d08178981ae80ea23a9b47f1b0dabbfceb846
denchen11214 <dchen22@stuy.edu>
2019-03-13 17:04:05 -0400

    finished

--- 10_svg/svg01.js
+++ 10_svg/svg01.js
@@ -1,33 +1,53 @@
 //Dennis Chen
 //SoftDev pd7
-//K #09: Connect the Dots
-//3/12/19
+//K #10: Connect the Dots
+//3/13/19
 
 var pic = document.getElementById("vimage");
+var clicked = false;
 pic.addEventListener('click', function(e){
     var x = e.offsetX;
     var y = e.offsetY;
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    //sets attribute of circle
-    c.setAttribute("cx",x);
-    c.setAttribute("cy",y);
-    c.setAttribute("r",50);
-    c.setAttribute("fill","blue");
-    c.setAttribute("stroke","black");
-    c.addEventListener('click', function(e){ //move out of this event listener
-      if(c.getAttribute('fill') == "blue"){
-        c.setAttribute('fill','yellow')
-      }
-      else{
-        c.setAttribute('fill','blue')
-        c.setAttribute('cx', 12) //replace w/random
-        c.setAttribute('cy', 12)
-      }
+    //checks if you clicked a circle
+    if(!circleClicked()){
+      //if not, create a circle
+      var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+      c.setAttribute("cx",x);
+      c.setAttribute("cy",y);
+      c.setAttribute("r",50);
+      c.setAttribute("fill","blue");
+      c.setAttribute("stroke","black");
+      pic.appendChild(c);
+      //add eventlistener to it
+      addClick();
     }
-    //adds path and dots to child nodes
-    pic.appendChild(c);
-    //updates oldX and oldY
+    else{
+      //sets clicked to false to reset after clicking the circle
+      clicked = false;
+    }
 })
+function addClick(){
+  //adds event listener to circle- makes it turn yellow or move to a random spot
+  child = pic.lastChild;
+  child.addEventListener('click',function(e){
+    if(this.getAttribute('fill') == "blue"){
+      this.setAttribute('fill','yellow')
+    }
+    else{
+      this.setAttribute('fill','blue')
+      this.setAttribute('cx', Math.random() * 500)
+      this.setAttribute('cy', Math.random() * 500)
+    }
+    clicked = true;
+  })
+}
+function circleClicked(){
+  //if no circles on the svg, return false, else return clicked
+  if(pic.children.length == 0){
+    return false;
+  }
+  return clicked
+}
 var clear = document.getElementById("but_clear")
 clear.addEventListener("click", function(e){
     //while the svg has children, keep removing the first one
