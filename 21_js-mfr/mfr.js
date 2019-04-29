02dd713fdd47bf7fd25926c209081895cc354802
DenChen11214 <dchen22@stuy.edu>
2019-04-29 13:18:45 -0400

    got it to read the json

---
+++ 21_js-mfr/mfr.js
@@ -0,0 +1,10 @@
+function readJSON(file) {
+    var request = new XMLHttpRequest();
+    request.open('GET', file, false);
+    request.send(null);
+    if (request.status == 200)
+        return request.responseText;
+};
+
+var temp = readJSON('data.json');
+console.log(temp)
