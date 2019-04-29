8ac099904c94c9cb2ee4bd2304cfa0b4640c7234
denchen11214 <dchen22@stuy.edu>
2019-04-29 18:39:24 -0400

    finished

--- 21_js-mfr/mfr.js
+++ 21_js-mfr/mfr.js
@@ -6,5 +6,37 @@ function readJSON(file) {
         return request.responseText;
 };
 
-var temp = readJSON('data.json');
-console.log(temp)
+var temp = JSON.parse(readJSON('data.json'));
+var asian = function(e){
+  var numStu = temp.reduce(function(a,b) {
+    if(b.asian_num == "0"){
+      return a
+    }
+    return a + b.asian_num
+  },0)
+  return numStu
+}
+console.log(asian())
+
+var numSchoolM = function(e){
+  var num = temp.filter(function(n){
+    return (n.male_per > 60) && (n.male_num > 1000)
+  })
+  return num.length
+}
+console.log(numSchoolM())
+
+var racePer = function(e){
+  var fit = temp.filter(function(n){
+    return n.asian_per > 20 && n.hispanic_per > 20 && n.white_per > 20 && n.black_per > 20
+  })
+  return fit.length / temp.length * 100
+}
+console.log(racePer())
+
+var asianNum = document.getElementById("asian")
+asianNum.innerHTML = asian()
+var numSchool = document.getElementById("male")
+numSchool.innerHTML = numSchoolM()
+var ethnicity = document.getElementById("ethnic")
+ethnicity.innerHTML = racePer()
