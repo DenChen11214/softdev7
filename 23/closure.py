805a31e58a2ca85d8f9b29f6c64200eae8926918
DenChen11214 <dchen22@stuy.edu>
2019-05-01 13:19:14 -0400

    started

---
+++ 23/closure.py
@@ -0,0 +1,30 @@
+import random
+def make_HTML_heading(f):
+    txt = f()
+    #print(txt)
+    def inner():
+        #print(txt)
+        return '<h1>' + txt + '</h1>'
+    return inner
+#equiv to greet = makeHTMLHeading(greet)
+@make_HTML_heading
+def greet():
+    greetings = ['hello','welcome','ayo','hola','bonjour','word up']
+    return random.choice(greetings)
+print(greet())
+
+def memoize(f):
+    memo = {}
+    def helper(x):
+        if x not in memo:
+        memo[x] = f()
+    return helper
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+fib = memoize(fib)
+print(fib(40))
